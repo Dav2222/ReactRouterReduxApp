a0a0a102184f139bc7150c028bb8a484e503f88d
Dav2222 <79914852+Dav2222@users.noreply.github.com>
2021-07-07 19:41:53 +0400

    Changed userId to userName in posts page

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,41 +1,56 @@
 import { useState, useEffect } from "react";
-import { LoadingOutlined, StarFilled} from "@ant-design/icons";
+import { LoadingOutlined, StarFilled } from "@ant-design/icons";
 import { Table, Popconfirm } from "antd";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import {createAddFavorite, createRemoveFavorite} from "../../store/actions"
-
+import { createAddFavorite, createRemoveFavorite } from "../../store/actions";
 
 const Posts = (props) => {
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
-const toggleFavorite = (record) => {
-  if(record.favorite){
-    props.dispatch(createRemoveFavorite(record.key)); 
-  }
-  else{
-    props.dispatch(createAddFavorite(record));
-  }
-  
-  setPosts(posts.map((e) => (e.key === record.key ? {...record, favorite: !e.favorite} : e)));
-}
+  const toggleFavorite = (record) => {
+    if (record.favorite) {
+      props.dispatch(createRemoveFavorite(record.key));
+    } else {
+      props.dispatch(createAddFavorite(record));
+    }
 
-  const setData = (json) => {
-    let arr = json.map((post) => ({
+    setPosts(
+      posts.map((e) =>
+        e.key === record.key ? { ...record, favorite: !e.favorite } : e
+      )
+    );
+  };
+
+  const setData = (posts, usersMap) => {
+    let arr = posts.map((post) => ({
       name: post.title,
-      author: post.userId,
+      author: usersMap[post.userId],
       key: post.id,
-      favorite: props.favorits.some((e) => (e.key === post.id) )
+      favorite: props.favorits.some((e) => e.key === post.id),
     }));
     setPosts(arr);
     setLoading(false);
   };
 
+  const FetchData = async () => {
+    let req = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let posts = await req.json();
+
+    let req_user = await fetch("https://jsonplaceholder.typicode.com/users");
+    let res_user = await req_user.json();
+
+    let userMap = {};
+    res_user.forEach((element) => {
+      userMap[element.id] = element.name;
+    });
+
+    setData(posts, userMap);
+  };
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    FetchData();
   }, []);
 
   const columns = [
@@ -60,9 +75,12 @@ const toggleFavorite = (record) => {
             title="Sure to delete?"
             onConfirm={() => alert(record.key)}
           >
-            <a>Delete</a>           
+            <a>Delete</a>
           </Popconfirm>
-          <StarFilled   style = {{color: record.favorite ? "red" : "blue"}}  onClick = {() => toggleFavorite(record)} />
+          <StarFilled
+            style={{ color: record.favorite ? "red" : "blue" }}
+            onClick={() => toggleFavorite(record)}
+          />
         </>
       ),
     },
@@ -71,18 +89,17 @@ const toggleFavorite = (record) => {
   return (
     <>
       <h1>Posts</h1>
-        {loading ? (
+      {loading ? (
         <LoadingOutlined />
       ) : (
-        <Table dataSource={posts} columns={columns}  />
+        <Table dataSource={posts} columns={columns} />
       )}
     </>
   );
 };
 
 const mapStateToProps = (state) => ({
-  favorits : state.favoritsList
-})
-
+  favorits: state.favoritsList,
+});
 
 export default connect(mapStateToProps)(Posts);
