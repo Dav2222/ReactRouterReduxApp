3764e23bff73a2a966f17bb8b62cef0a7a2179c7
GIT-MD <minasdavtyan@gmail.com>
2021-07-07 21:10:36 +0400

    added filter functionality

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -4,10 +4,17 @@ import { Table, Popconfirm } from "antd";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createAddFavorite, createRemoveFavorite } from "../../store/actions";
+import { Input, Row, Select, Button, Col } from "antd";
+
+const { Option } = Select;
 
 const Posts = (props) => {
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
+  const [filterColumn, setFilterColumn] = useState("");
+  const [filterStatement, setFilterStatement] = useState("");
+  const [authors, setAuthors] = useState({});
+  const [filteredPosts, setFilteredPosts] = useState({});
 
   const toggleFavorite = (record) => {
     if (record.favorite) {
@@ -31,6 +38,7 @@ const Posts = (props) => {
       favorite: props.favorits.some((e) => e.key === post.id),
     }));
     setPosts(arr);
+    setFilteredPosts(arr);
     setLoading(false);
   };
 
@@ -46,6 +54,7 @@ const Posts = (props) => {
       userMap[element.id] = element.name;
     });
 
+    setAuthors(userMap);
     setData(posts, userMap);
   };
 
@@ -86,13 +95,106 @@ const Posts = (props) => {
     },
   ];
 
+  const formatString = (string) => {
+    return string.trim().toLowerCase();
+  };
+
+  const applyFilter = () => {
+    if (filterStatement.length === 0) {
+      setFilteredPosts(posts);
+      return;
+    }
+    if (filterColumn.length === 0) {
+      setFilteredPosts(
+        posts.filter((post) => {
+          for (let i = 0; i < columns.length; i++) {
+            if (
+              columns[i].key !== "x" &&
+              formatString(post[columns[i].key]).includes(
+                formatString(filterStatement)
+              )
+            ) {
+              return true;
+            }
+          }
+          return false;
+        })
+      );
+      return;
+    }
+    if (formatString(filterStatement) && filterColumn) {
+      setFilteredPosts(
+        posts.filter((post) =>
+          formatString(post[filterColumn]).includes(
+            formatString(filterStatement)
+          )
+        )
+      );
+      return;
+    }
+  };
+
   return (
     <>
       <h1>Posts</h1>
       {loading ? (
         <LoadingOutlined />
       ) : (
-        <Table dataSource={posts} columns={columns} />
+        <>
+          <Row>
+            <Col span="24">
+              <Input.Group compact>
+                <Select
+                  style={{ width: "150px" }}
+                  defaultValue={filterColumn}
+                  onChange={(value) => setFilterColumn(value)}
+                >
+                  <Option value="">All</Option>
+                  {columns.map((e) =>
+                    e.key !== "x" ? (
+                      <Option key={e.key} value={e.key}>
+                        {e.title}
+                      </Option>
+                    ) : (
+                      ""
+                    )
+                  )}
+                </Select>
+                {filterColumn === "author" ? (
+                  <Select
+                    style={{ width: "calc(50% - 250px)" }}
+                    defaultValue={filterStatement}
+                    onChange={(value) => setFilterStatement(value)}
+                  >
+                    {Object.values(authors).map((e) => (
+                      <Option key={e} value={e}>
+                        {e}
+                      </Option>
+                    ))}
+                  </Select>
+                ) : (
+                  <Input
+                    style={{ width: "calc(50% - 250px)" }}
+                    defaultValue={filterStatement}
+                    onChange={(evt) => setFilterStatement(evt.target.value)}
+                  />
+                )}
+                <Button
+                  style={{ width: "100px" }}
+                  type="primary"
+                  onClick={applyFilter}
+                >
+                  Filter
+                </Button>
+              </Input.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col span="24">
+              <Table dataSource={filteredPosts} columns={columns} />
+            </Col>
+          </Row>
+        </>
       )}
     </>
   );
